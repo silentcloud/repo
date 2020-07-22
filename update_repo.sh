50fa930c0f324bbfb4d60d58654539cd56504204
Hayden <me@diatr.us>
2020-07-22 15:00:27 -0400

    Make script better for Linux

--- update_repo.sh
+++ update_repo.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 cd $(dirname "$0")
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+    FTPARCHIVE='apt-ftparchive'
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+    FTPARCHIVE='./apt-ftparchive'
+fi
 for dist in appletvos-arm64/1300 iphoneos-arm64/1{5,6}00 watchos-arm64/1400 watchos-arm/1400; do
     if [[ "${dist}" == "iphoneos-arm"* ]]; then
         arch=iphoneos-arm
@@ -13,13 +18,13 @@ for dist in appletvos-arm64/1300 iphoneos-arm64/1{5,6}00 watchos-arm64/1400 watc
     rm -f dists/${dist}/{Release{,.gpg},main/${binary}/{Packages{,.xz,.zst},Release{,.gpg}}}
     cp -a CydiaIcon*.png dists/${dist}
 
-    ./apt-ftparchive packages pool/main/${dist} > \
+    $FTPARCHIVE packages pool/main/${dist} > \
                 dists/${dist}/main/${binary}/Packages 2>/dev/null
     xz -c9 dists/${dist}/main/${binary}/Packages > dists/${dist}/main/${binary}/Packages.xz
     zstd -q -c19 dists/${dist}/main/${binary}/Packages > dists/${dist}/main/${binary}/Packages.zst
     
-    ./apt-ftparchive release -c config/${arch}-basic.conf dists/${dist}/main/${binary} > dists/${dist}/main/${binary}/Release 2>/dev/null
-    ./apt-ftparchive release -c config/$(echo "${dist}" | cut -f1 -d '/').conf dists/${dist} > dists/${dist}/Release 2>/dev/null 
+    $FTPARCHIVE release -c config/${arch}-basic.conf dists/${dist}/main/${binary} > dists/${dist}/main/${binary}/Release 2>/dev/null
+    $FTPARCHIVE release -c config/$(echo "${dist}" | cut -f1 -d '/').conf dists/${dist} > dists/${dist}/Release 2>/dev/null
 
     gpg -abs -u C59F3798A305ADD7E7E6C7256430292CF9551B0E -o dists/${dist}/Release.gpg dists/${dist}/Release
 done
